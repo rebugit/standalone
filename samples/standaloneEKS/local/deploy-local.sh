59ab38ce5e6725562876d3d9bc1a61aca309ed8d
MatteoGioioso <gioioso.matteo@gmail.com>
2021-07-19 09:31:01 +0700

    fix linkerd keycloak issue

---
+++ samples/standaloneEKS/local/deploy-local.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Create kind cluster
+kind create cluster --config=cluster.yaml
+
+# Install nginx ingress
+echo "Installing nginx ingress controller"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml &&
+echo "Waiting for nginx resources to be ready"
+sleep 30 &&
+echo "Waiting for nginx ingress"
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s &&
+
+# Setup Linkerd
+echo "Installing Linkerd"
+linkerd check --pre &&
+linkerd install | kubectl apply -f - &&
+linkerd check &&
+linkerd viz install | kubectl apply -f -
+echo "Linkerd installed"
